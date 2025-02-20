9f01ee249a6452971917cfb908788175431ac999
KJR-dev <sahooj168@gmail.com>
2025-02-20 12:33:59 +0530

    add sanitization

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,9 +1,32 @@
 import { checkSchema } from 'express-validator';
 
 export default checkSchema({
+    firstName: {
+        in: ['body'],
+        errorMessage: 'First name is required!',
+        trim: true,
+        notEmpty: {
+            errorMessage: 'First name cannot be empty!',
+        },
+        isAlpha: {
+            errorMessage: 'First name should be character!',
+        },
+    },
+    lastName: {
+        in: ['body'],
+        errorMessage: 'Last name is required!',
+        trim: true,
+        notEmpty: {
+            errorMessage: 'Last name cannot be empty!',
+        },
+        isAlpha: {
+            errorMessage: 'Last name should be character!',
+        },
+    },
     email: {
-        in: ['body'], // Ensures validation applies to req.body
+        in: ['body'],
         errorMessage: 'Email is required!',
+        trim: true,
         notEmpty: {
             errorMessage: 'Email cannot be empty!',
         },
--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -7,19 +7,20 @@ import { User } from '../../src/entity/User';
 import { Roles } from '../../src/constants';
 
 describe('post /auth/register', () => {
+    let connection: DataSource;
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        await connection.dropDatabase();
+        await connection.synchronize();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
     describe('Happy Parts', () => {
-        let connection: DataSource;
-        beforeAll(async () => {
-            connection = await AppDataSource.initialize();
-        });
-        beforeEach(async () => {
-            await connection.dropDatabase();
-            await connection.synchronize();
-            // await truncateTables(connection);
-        });
-        afterAll(async () => {
-            await connection.destroy();
-        });
         it('Should return the 201 status code', async () => {
             //Arrenge
             const userData = {
@@ -88,6 +89,27 @@ describe('post /auth/register', () => {
                 //Assert
                 expect(response.statusCode).toBe(400);
             });
+        });
+    });
+    describe('Sanitizing fields', () => {
+        it('should trim the email field', async () => {
+            //Arrenge
+            const userData = {
+                firstName: 'Jitendra',
+                lastName: 'Sahoo',
+                email: ' sahooj168@gmail.com ',
+                password: '1234',
+            };
+
+            //Act
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            await request(app).post('/api/v1/web/auth/register').send(userData);
+
+            //Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            const user = users[0];
+            expect(user.email).toBe('sahooj168@gmail.com');
         });
     });
 });
