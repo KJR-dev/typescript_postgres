de60094972d590abec0529a3aa41080d644cd2f8
KJR-dev <sahooj168@gmail.com>
2025-02-20 09:15:32 +0530

    testing unique email

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -8,35 +8,23 @@ import bcrypt from 'bcrypt';
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: UserData) {
-        try {
-            const existUser = await this.userRepository.findOne({
-                where: { email: email },
-            });
+        const existUser = await this.userRepository.findOne({
+            where: { email: email },
+        });
 
-            if (existUser) {
-                const err = createHttpError(400, 'Email is already exist');
-                throw err;
-            }
-            const saltRound = 10;
-            const hashedPassword = await bcrypt.hash(password, saltRound);
-            const user = await this.userRepository.save({
-                firstName,
-                lastName,
-                email,
-                password: hashedPassword,
-                role: Roles.CUSTOMER,
-            });
-            return user;
-        } catch (err) {
-            if (createHttpError.isHttpError(err)) {
-                throw err; // Preserve the original error status
-            }
-            throw createHttpError(
-                500,
-                err instanceof Error
-                    ? err.message
-                    : 'Failed to store the data in the database',
-            );
+        if (existUser) {
+            const err = createHttpError(400, 'Email is already exist');
+            throw err;
         }
+        const saltRound = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRound);
+        const user = await this.userRepository.save({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            role: Roles.CUSTOMER,
+        });
+        return user;
     }
 }
