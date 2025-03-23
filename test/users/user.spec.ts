362ea2f3c2b2078669de140603f2ef91d87e21ef
KJR-dev <sahooj168@gmail.com>
2025-03-24 01:04:28 +0530

    Token Verification - User Data Testing

--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -2,17 +2,27 @@ import request from 'supertest';
 import { DataSource } from 'typeorm';
 import { AppDataSource } from '../../src/config/data-source';
 import app from '../../src/app';
+import { User } from '../../src/entity/User';
+import { Roles } from '../../src/constants';
+import createJWKSMock from 'mock-jwks';
 
 describe('POST /auth/self', () => {
     let connection: DataSource;
+    let jwks: ReturnType<typeof createJWKSMock>;
 
     beforeAll(async () => {
+        jwks = createJWKSMock('http://localhost:5501');
         connection = await AppDataSource.initialize();
     });
 
     beforeEach(async () => {
+        jwks.start();
         await connection.dropDatabase();
         await connection.synchronize();
+    });
+
+    afterEach(() => {
+        jwks.stop();
     });
 
     afterAll(async () => {
@@ -31,6 +41,32 @@ describe('POST /auth/self', () => {
 
             //Asserts
             expect(response.statusCode).toBe(200);
+        });
+
+        it('Should return user data', async () => {
+            const userData = {
+                firstName: 'Jitendra',
+                lastName: 'sahoo',
+                email: 'sahooj168@gmail.com',
+                password: '1234',
+            };
+
+            const userRepository = connection.getRepository(User);
+            const data = await userRepository.save({
+                ...userData,
+                role: Roles.CUSTOMER,
+            });
+            const accessToken = jwks.token({
+                sub: String(data.id),
+                role: data.role,
+            });
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            const response = await request(app)
+                .get('/auth/self')
+                .set('Cookie', [`accessToken=${accessToken}`])
+                .send();
+
+            expect((response.body as Record<string, string>).id).toBe(data.id);
         });
     });
 });
