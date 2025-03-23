5507605aa16dfdf93d16271b27462ab266325c97
KJR-dev <sahooj168@gmail.com>
2025-03-23 13:18:12 +0530

    login test case done

--- test/users/login.spec.ts
+++ test/users/login.spec.ts
@@ -2,7 +2,8 @@ import request from 'supertest';
 import { DataSource } from 'typeorm';
 import { AppDataSource } from '../../src/config/data-source';
 import app from '../../src/app';
-// import { User } from '../../src/entity/User';
+import { User } from '../../src/entity/User';
+import { CredentialService } from '../../src/services/CredentialService';
 
 describe('POST /auth/login', () => {
     let connection: DataSource;
@@ -24,6 +25,8 @@ describe('POST /auth/login', () => {
         it('should login the user', async () => {
             //Arrenge
             const userData = {
+                firstName: 'Jitendra',
+                lastName: 'Sahoo',
                 email: 'sahooj168@gmail.com',
                 password: '1234',
             };
@@ -33,9 +36,23 @@ describe('POST /auth/login', () => {
             await request(app).post('/api/v1/web/auth/register').send(userData);
 
             //Asserts
-            // const userRepository = connection.getRepository(User);
-            // const users = await userRepository.find();
-            // expect(users).toHaveLength(1);
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find({
+                where: {
+                    email: userData.email,
+                },
+            });
+
+            expect(users).toHaveLength(1);
+
+            console.log(users[0].password);
+            const credentialService = new CredentialService(); // Create an instance
+            const result = await credentialService.comparePassword(
+                userData.password,
+                users[0].password,
+            );
+
+            expect(result).toBe(true);
         });
     });
 });
